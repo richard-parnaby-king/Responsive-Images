ff87412740c4cfed28d851364a04c47beb5e4fb1
Richard Parnaby-King <richard.parnaby-king@clickconsult.com>
2014-02-05 15:36:58 +0000

    Made code into a plugin and updated readme instructions

--- readme.md
+++ readme.md
@@ -16,10 +16,15 @@ A proposal for the HTML5 specification was the <picture> element:
 ```
 As no browser supports the picture element, the above code will not be rendered except for the img element.
 
-This jQuery plugin self-executes to replace picture elements with an img pointing to the appropriate image source.
+This jQuery plugin self-executes to replace picture elements with an img pointing to the appropriate image source. It will use the first picture source with a valid media query, so it is strongly recommended to place the largest image source first. If the picture source does not have a media attribute, the plugin shall set it as having min-width:0 (show on all browser sizes).
 
 # Usage
 ```html
 <script type="text/javascript" src="/path/to/jquery.html5pictures.js"></script>
 ```
+
+**Do not include the script directly from GitHub (http://raw.github.com/...).** The file is being served as text/plain and as such being blocked in Internet Explorer on Windows 7 for instance (because of the wrong MIME type). Bottom line: GitHub is not a CDN.
+
+Use the above html with the largest image first scaling down.
+
 This plugin self-executes so there is no need to call any init functions.
--- responsivePictures.js
+++ responsivePictures.js
@@ -1,41 +1,53 @@
+/*!
+ * jQuery Fader Plugin v1
+ * https://github.com/richard-parnaby-king/Responsive-Images
+ *
+ * @Copyright 2013 Richard Parnaby-King
+ * @author Richard Parnaby-King
+ * Released under the MIT license
+ */
+(function($){
+  "use strict";
+  $.fn.responsivePictures = function(){
+    //only run this code if window.matchMedia is available
+    if(window.matchMedia) {
+      this.each(function() {
+      
+      //replace all <picture> elements with <img> elements with correct src
+          //replacement image:
+          var img = document.createElement('img');
 
-  // Enable strict mode
-	"use strict";
+          //find image src that matches media command
+          var src = $(this).find('source');
+          src.each(function(){
+            var s = $(this);
+            
+            //if no media attribute located, set it to apply to all screen
+            if(s.attr('media') === undefined) {
+              s.attr('media','(min-width:0)');
+            }
+            
+            //If media query is valid, then set the source url
+            if(window.matchMedia(s.attr('media')).matches) {
+              img.src = s.attr('src');
+              return false;
+            }
+          });
 
-  //responsive images
-  //replace all <picture> elements with <img> elements with correct src
-  if(window.matchMedia) {
-    $('picture').each(function(){
-      //replacement image:
-      var img = document.createElement('img');
+          //Not found a valid image. Stop processing
+          if(img.src.length === 0) {
+            return;
+          }
 
-      //find image src that matches media command
-      var src = $(this).find('source');
-      src.each(function(){
-        var s = $(this);
-        
-        //if no media attribute located, set it to apply to all screen
-        if(s.attr('media') === undefined) {
-          s.attr('media','(min-width:0)');
-        }
-        
-        //If media query is valid, then set the source url
-        if(window.matchMedia(s.attr('media')).matches) {
-          img.src = s.attr('src');
-          return false;
-        }
-      });
+          //Set the alt text (good for SEO)
+          img.alt = $(this).find('img').attr('alt');
 
-      //Not found a valid image. Stop processing
-      if(img.src.length === 0) {
-        return;
-      }
-
-      //Set the alt text (good for SEO)
-      img.alt = $(this).find('img').attr('alt');
-
-      //replace picture with image
-      $(this).replaceWith(img);
-    });
+          //replace picture with image
+          $(this).replaceWith(img);
+        });
+    } else {
+      console.log('window.matchMedia is not supported in this browser');
+    }
   }
-  //end responsive images
+  $('picture').responsivePictures();
+})(jQuery);
